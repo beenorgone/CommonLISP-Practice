31ec729c55fe4f6523bae0d6bdc14e5151efe46e
beenorgone <manhha.tuan@gmail.com>
2014-01-11 21:45:48 +0700

    - ‘.Num2Text.lisp.kate-swp’ / ‘Num2Text.lisp’

--- Num2Text.lisp
+++ Num2Text.lisp
@@ -23,8 +23,10 @@ Viết hàm 3NUMS-GROUPS-2-TEXT để chuyển các bộ 3 số thành dạng te
 (defun rest-3nums-groups-2-text (nums)
        (cond ((zerop (length nums)) nil)
 	     (t (append (3nums-2-text (first nums))
-			(cons (add-unit (length nums))
-			      (rest-3nums-groups-2-text (rest nums)))))))
+			(list (if (zerop (first nums))
+				  nil
+				  (add-unit (length nums))))
+			(rest-3nums-groups-2-text (rest nums))))))
 
 (setf text-4-units
       '((5 nghìn-tỷ)

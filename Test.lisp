10a76ec9bae8f4318faf16ed62b23458ac570d25
beenorgone <manhha.tuan@gmail.com>
2014-01-13 12:29:09 +0700

    / ‘.Test.lisp.kate-swp’ / ‘Test.lisp’

--- Test.lisp
+++ Test.lisp
@@ -14,17 +14,32 @@
 
 (defun groups-of-nine->words (nums)
        (if (equalp 1 (length nums))
-	   (trios->words (group-of-nine->trios nums))
+	   (trios->words (num->trios nums))
 	   (append (first-group-of-nine->words (first nums))
 		   '(tỷ)
 		   "(trios->words (groups-of-nine->trios (rest nums))"
 		   (rest-groups-of-nine->words (rest nums))
 		   )))
 
-(defun group-of-nine->trios (num)
+(defun num->trios (num)
        (cond ((zerop (floor num 1000)) (list num))
-	     (t (append (group-of-nine->trios (floor num 1000))
+	     (t (append (num->trios (floor num 1000))
 			(list (mod num 1000))))))
+
+(defun trios->words (nums)
+       (append (first-trio->words (first nums))
+	       (list (add-unit (length nums)))
+	       (rest-trios->words (rest nums))))
+
+(setf unit-table
+      '((3 triệu)
+	(2 nghìn)
+	(1 nil)))
+
+(defun add-unit (a)
+       (second (assoc a unit-table)))
+
+(defun)
 
 (defun rest-groups-of-nine->trios (nums)
        )

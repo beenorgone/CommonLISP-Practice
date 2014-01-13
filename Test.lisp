5f34938121924674b81d26079ea75572c03d2c67
beenorgone <manhha.tuan@gmail.com>
2014-01-13 12:33:38 +0700

    - ‘.Num2Text.lisp.kate-swp’ - ‘.Test.lisp.kate-swp’ / ‘Test.lisp’

--- Test.lisp
+++ Test.lisp
@@ -1,5 +1,5 @@
-(defun num->words (num)
-       (append (groups-of-nine->words (num->groups-of-nine num))
+(defun num->word (num)
+       (append (groups-of-nine->word (num->groups-of-nine num))
 	       measure-unit))
 
 (setf measure-unit '(đơn vị)) ;replace đơn-vị with m2, m3, vnd, usd, ...
@@ -12,13 +12,13 @@
 "Test cases"
 (num->groups-of-nine 1000234003) ;(1 234003)
 
-(defun groups-of-nine->words (nums)
+(defun groups-of-nine->word (nums)
        (if (equalp 1 (length nums))
-	   (trios->words (num->trios nums))
-	   (append (first-group-of-nine->words (first nums))
+	   (trios->word (num->trios nums))
+	   (append (first-group-of-nine->word (first nums))
 		   '(tỷ)
-		   "(trios->words (groups-of-nine->trios (rest nums))"
-		   (rest-groups-of-nine->words (rest nums))
+		   "(trios->word (groups-of-nine->trios (rest nums))"
+		   (rest-groups-of-nine->word (rest nums))
 		   )))
 
 (defun num->trios (num)
@@ -26,10 +26,10 @@
 	     (t (append (num->trios (floor num 1000))
 			(list (mod num 1000))))))
 
-(defun trios->words (nums)
-       (append (first-trio->words (first nums))
+(defun trios->word (nums)
+       (append (first-trio->word (first nums))
 	       (list (add-unit (length nums)))
-	       (rest-trios->words (rest nums))))
+	       (rest-trios->word (rest nums))))
 
 (setf unit-table
       '((3 triệu)
@@ -39,7 +39,23 @@
 (defun add-unit (a)
        (second (assoc a unit-table)))
 
-(defun)
+(defun first-trio->word (num)
+       (cond ((< num 10)
+	      (list (second (assoc num num->word-table))))
+	     ((< num 100 (append (second-digit->word num)
+				 (third-digit->word))))))
+
+(setf num->word-table
+      '((0 không (linh)) ;linh v lẻ
+	(1 một (mười) mốt)
+	(2 hai (hai mươi) hai)
+	(3 ba (ba mươi) ba)
+	(4 bốn (bốn mươi) tư)
+	(5 năm (năm mươi) lăm)
+	(6 sáu (sáu mươi) sáu)
+	(7 bảy (bảy mươi) bảy)
+	(8 tám (tám mươi) tám)
+	(9 chín (chín mươi) chín)))
 
 (defun rest-groups-of-nine->trios (nums)
        )

b341be024747917604c8152af9a9a3e5aff8d6f6
beenorgone <manhha.tuan@gmail.com>
2014-01-13 10:38:09 +0700

    - ‘.Test.lisp.kate-swp’ / ‘Test.lisp’

--- Test.lisp
+++ Test.lisp
@@ -4,3 +4,7 @@
 
 (setf measure-unit '(đơn vị))
 
+(defun num->groups-of-nine (num) ;(num->groups-of-nine 1000234003) -> (1 234003)
+       (cond ((zerop (floor num 1000000000)) (list num))
+	     (t (append (num->groups-of-nine (floor num 1000000000))
+			(list (mod num 1000000000))))))
